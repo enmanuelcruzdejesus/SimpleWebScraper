c219de2d12522ed15ab94486ce7f6cce36d3deb1
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2024-04-05 17:20:56 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HtmlAgilityPack;
+
+Console.WriteLine("Hello, World!");
+
+
+// creating the HAP object 
+var web = new HtmlWeb(); 
+
+// visiting the target web page 
+var document = web.Load("https://fc.dgii.gov.do/ecf/ConsultaTimbreFc?RncEmisor=101155949&ENCF=E320000000681&MontoTotal=1495.00&CodigoSeguridad=Wmm9MP"); 
+
+var tags = document.DocumentNode.QuerySelectorAll("th.text-left");
+
+Console.WriteLine("");
+
